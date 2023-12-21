070f20b93a80b40e1ec21f166c12b1aadebe7033
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-12-20 23:04:00 -0600

    Implement Auth Action to create a new user

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,42 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+//Triggered whenever the form is submitted
+export const action = async ({ request }) => {
+  //Get current url params by default browser features
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode" }, { status: 422 });
+  }
+  //Get acces to the form data that was submitted
+  const data = await request.formData();
+  //Get method existis on the data object returned by formData
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(authData),
+  });
+
+  //If validation errors
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user" }, { status: 500 });
+  }
+
+  //soon: manage the token
+  return redirect("/");
+};
